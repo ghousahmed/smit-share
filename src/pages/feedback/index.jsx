a7e4c0425b70abd809a3eecb72e8c2f20c6e7715
ahmedraza <ahmed.raza1054@gmail.com>
2024-02-14 11:55:31 +0500

    fix feedback and other pages issues

--- src/pages/feedback/index.jsx
+++ src/pages/feedback/index.jsx
@@ -2,22 +2,18 @@ import "./style.scss";
 import "./mediaQuery.scss";
 import {
   useTheme,
-  useScreenWidth,
-  useEffect,
+  // useScreenWidth,
+  // useEffect,
   useState,
-  LOGO,
-  FiMenu,
-  MdLightMode,
-  MdDarkMode,
   Link,
+  Navbar,
+  Footer,
 } from "../../components/index";
 import sendFeedback from "../../firebase/sendFeedback.js";
 import send from "./../../assets/send.webp";
 
 function Feedback({ login }) {
-  const { isDark, toggleTheme } = useTheme();
-  const screenWidth = useScreenWidth();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { isDark } = useTheme();
   const [isSubmitted, setSubmitState] = useState(false);
   const [isLoading, setLoader] = useState(false);
   const [formDataErrors, setFormDataErrors] = useState({});
@@ -25,18 +21,6 @@ function Feedback({ login }) {
     email: "",
     textArea: "",
   });
-
-  useEffect(() => {
-    updateBodyClass();
-  }, [isDark]);
-
-  const updateBodyClass = () => {
-    document.body.classList.toggle("dark", isDark);
-  };
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -80,133 +64,7 @@ function Feedback({ login }) {
 
   return (
     <div className={`container ${isDark ? "dark" : ""}`}>
-      <div className="header-bar">
-        <div className="logo">
-          <Link to="/">
-            <img src={LOGO} alt="icon" />
-          </Link>
-        </div>
-        <div className="menu-bar">
-          {screenWidth.widthScreen > 768 ? (
-            <ul>
-              <li className={isDark ? "dark-text" : " "}>
-                <Link
-                  to="/how-it-works"
-                  className={isDark ? "dark-text" : " "}
-                  style={{
-                    textDecoration: "none",
-                    color: isDark ? "#4a4a4a" : "",
-                  }}
-                >
-                  How it works
-                </Link>
-              </li>
-              <li className={isDark ? "dark-text" : " "}>Download</li>
-              <li className={isDark ? "dark-text" : " "}>Upgrade</li>
-              <li className={isDark ? "dark-text" : " "}>
-                <Link
-                  to="/feedback"
-                  className={isDark ? "dark" : " "}
-                  style={{
-                    textDecoration: "none",
-                    color: "#252526",
-                  }}
-                >
-                  Feedback
-                </Link>
-              </li>
-              <li className="menu-btn">
-                <Link
-                  to="/login"
-                  style={{ textDecoration: "none", color: "#638eff" }}
-                >
-                  Login
-                </Link>{" "}
-                /{" "}
-                <Link
-                  to="/signup"
-                  style={{ textDecoration: "none", color: "#638eff" }}
-                >
-                  Register
-                </Link>
-              </li>
-              <li onClick={toggleTheme}>
-                {isDark ? (
-                  <MdLightMode size={24} color="white" />
-                ) : (
-                  <MdDarkMode size={24} />
-                )}
-              </li>
-            </ul>
-          ) : (
-            <ul>
-              <li onClick={toggleMenu}>
-                <FiMenu size={30} />
-              </li>
-              <li onClick={toggleTheme}>
-                {isDark ? (
-                  <MdLightMode size={24} color="white" />
-                ) : (
-                  <MdDarkMode size={24} />
-                )}
-              </li>
-            </ul>
-          )}
-          {isMenuOpen ? (
-            <div className={`mobile-menu ${isDark ? "dark" : ""}`}>
-              <ul style={{ backgroundColor: isDark ? "#252526" : "#f4f4f4" }}>
-                <li className={isDark ? "dark-text" : ""}>
-                  <Link
-                    to="/how-it-works"
-                    style={{
-                      textDecoration: "none",
-                      color: isDark ? "#fff" : "#4a4a4aa6",
-                    }}
-                  >
-                    How it works
-                  </Link>
-                </li>
-                <li className={isDark ? "dark" : " "}>Download</li>
-                <li className={isDark ? "dark" : " "}>Upgrade</li>
-                <li className={isDark ? "dark" : " "} style={{ color: "#000" }}>
-                  Feedback
-                </li>
-                {login ? (
-                  <li className="menu-btn" onClick={logoutUser}>
-                    {t("Logout")}
-                  </li>
-                ) : (
-                  <li className="menu-btn">
-                    <span>
-                      {" "}
-                      <Link
-                        className="menu-btn"
-                        style={{ textDecoration: "none" }}
-                        to={"/login"}
-                      >
-                        {" "}
-                        Login{" "}
-                      </Link>
-                    </span>
-                    /{" "}
-                    <span>
-                      {" "}
-                      <Link
-                        className="menu-btn"
-                        to={"/signup"}
-                        style={{ textDecoration: "none" }}
-                      >
-                        {" "}
-                        Register{" "}
-                      </Link>
-                    </span>
-                  </li>
-                )}
-              </ul>
-            </div>
-          ) : null}
-        </div>
-      </div>
+      <Navbar />
 
       <div className="main-card">
         {isLoading ? (
@@ -217,12 +75,12 @@ function Feedback({ login }) {
           <div className={`feedback ${isDark ? "dark-light" : " "}`}>
             {!isSubmitted ? (
               <form>
-                <h2 className={`${isDark ? "dark-light" : " "}`}>
+                <h2 className={isDark ? "dark-light" : " "}>
                   Share your experience
                 </h2>
                 <div className={`fields ${isDark ? "dark-light" : " "}`}>
                   <span>
-                    <p className={`${isDark ? "dark-light" : " "}`}>Email</p>
+                    <p className={isDark ? "dark-light" : " "}>Email</p>
                     <input
                       value={formData.email}
                       name="email"
@@ -288,20 +146,7 @@ function Feedback({ login }) {
           </div>
         )}
       </div>
-      <div className="footer">
-        <span>
-          © 2023-2024 AirForShare.com <br />
-          Made in
-          <Link
-            className="link"
-            to="https://www.linkedin.com/company/saylanimasstraining/?originalSubdomain=pk"
-          >
-            {" "}
-            SMIT.com{" "}
-          </Link>
-          with ❤️
-        </span>
-      </div>
+      <Footer />
     </div>
   );
 }
